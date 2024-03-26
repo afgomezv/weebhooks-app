8236b6e5cdf56adec0a7a95334ab420cf574beec
afgomezv <afgomezvonline@gmail.com>
2024-03-26 12:30:30 -0500

    security webhooks

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { envs } from "./config";
 import { GitHubController } from "./presentacion/github/controller";
+import { GithunSha256Middleware } from "./presentacion/middlewares/github-sha256.middlewares";
 
 (() => {
   main();
@@ -12,6 +13,8 @@ function main() {
   const controlller = new GitHubController();
 
   app.use(express.json());
+
+  app.use(GithunSha256Middleware.verifySignature);
 
   app.post("/api/github", controlller.webHookHandler);
 
